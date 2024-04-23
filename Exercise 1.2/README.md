b275bf889b5e399198d2edbd7ab113b8e9e5cbc5
dwd369 <dwd7290@gmail.com>
2024-04-23 00:40:45 -0700

    adding exercise 1.2

---
+++ Exercise 1.2/README.md
@@ -0,0 +1,4 @@
+#Exercise 1.2
+## Goal of the exercise is to append multiple recipes into a single list
+## Reason I choose dictionary as individual recipe is because it allows the user to fetch the recipe's name, cook, time, or ingredient individually when needed
+## Reason I choose list as all_recipe is because it's convenience to fetch a specific item using index number, providing the ability to loop through each item to find a specific recipe
---
+++ Exercise 1.2/exercise.py
@@ -0,0 +1,38 @@
+recipe_1 = {
+    "name": "Tea",
+    "cooking_time": 5,
+    "ingredients": ["Tea leaves", "Sugar", "Water"]
+}
+
+all_recipes = []
+
+all_recipes.append(recipe_1)
+
+recipe_2 = {
+    "name": "Coffee",
+    "cooking_time": 5,
+    "ingredients": ["Cofee Beans", "Sugar", "Water"]
+}
+
+recipe_3 = {
+    "name": "Hot Chocolate",
+    "cooking_time": 5,
+    "ingredients": ["Coco Powder", "Water"]
+}
+
+recipe_4 = {
+    "name": "Arnald Palmer",
+    "cooking_time": 5,
+    "ingredients": ["Tea leaves", "Lemonade", "Water", "Ice"]
+}
+
+recipe_5 = {
+    "name": "Italian Soda",
+    "cooking_time": 5,
+    "ingredients": ["Flavored Syrup", "Club Soda / Sparkling Water"]
+}
+
+all_recipes.append(recipe_2)
+all_recipes.append(recipe_3)
+all_recipes.append(recipe_4)
+all_recipes.append(recipe_5)
